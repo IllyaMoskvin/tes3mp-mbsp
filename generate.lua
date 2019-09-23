66c67e37c0696bcc496ddf7035efa3eb6b4bb250
Illya Moskvin <ivmoskvin@gmail.com>
2019-09-23 02:12:03 -0500

    Implement parsing spells from mod files

--- generate.lua
+++ generate.lua
@@ -48,3 +48,83 @@ for i, file in pairs(config['files']) do
         os.exit(1)
     end
 end
+
+-- Adapted from `parseMiscs` in original `espParser.lua`
+-- https://github.com/JakobCh/tes3mp_scripts/blob/4096203/espParser/scripts/espParser.lua#L333
+function parseSpells(filename)
+    local records = espParser.getRecords(filename, "SPEL")
+
+    if espParser.files[filename] == nil then
+        espParser.files[filename] = {}
+    end
+
+    espParser.files[filename].spells = {}
+
+    local dataTypes = {
+        NAME = {"s", "refId"},
+        FNAM = {"s", "name"},
+        SPDT = {
+            {
+                {"i", "type"},
+                {"i", "cost"},
+                {"i", "flags"}
+            }, "data"
+        }
+        -- We don't care about ENAM
+    }
+
+    for _, record in pairs(records) do
+        local refId = struct.unpack( "s", record:getSubRecordsByName("NAME")[1].data )
+        espParser.files[filename].spells[refId] = {}
+
+        for _, subrecord in pairs(record.subRecords) do
+            espParser.files[filename].spells[refId] = espParser.subrecordParseHelper(espParser.files[filename].spells[refId], dataTypes, subrecord)
+        end
+    end
+end
+
+-- Start building our table for output
+local spells = {}
+
+-- Use our modified espParser to load all magic effects
+-- https://github.com/JakobCh/tes3mp_scripts/blob/4096203/espParser/scripts/espParser.lua#L398
+for i, file in pairs(config['files']) do
+    print("Loading " .. file)
+
+    if not espParser.addEsp(file) then
+        print("Failed to load: " .. file)
+        os.exit(1)
+    end
+
+    parseSpells(file)
+
+    -- https://github.com/JakobCh/tes3mp_scripts/blob/b703731/espParser/scripts/espParserTest.lua
+    for _, spell in pairs(espParser.files[file].spells) do
+        -- type: 0 = Spell, see https://en.uesp.net/morrow/tech/mw_esm.txt
+        if spell.data.type == 0 then
+            spells[spell.refId] = spell.data.cost
+        end
+    end
+end
+
+-- Helper to sort spells alphabetically when outputting to JSON
+local spellIds = {}
+local spellCount = 0
+
+for spellId, spellCost in pairs(spells) do
+    spellIds[spellCount] = spellId
+    spellCount = spellCount + 1
+end
+
+table.sort(spellIds)
+
+-- Output our spell list to file
+local file = io.open("spells/custom.json", "w+b")
+
+if file then
+    local content = dkjson.encode(spells, { indent = true, keyorder = spellIds })
+    file:write(content)
+    file:close()
+end
+
+print("Success! " .. tostring(spellCount) .. " spells written to `spells/custom.json`")
